8ab0956a2bca5c0b39ae4ca7529de305c6ee191c
weimizhoujoyce@gmail.com <weimizhoujoyce@gmail.com>
2021-04-09 17:04:09 +0930

    new version4.9-2

--- resources/js/components/commons/Showcard.vue
+++ resources/js/components/commons/Showcard.vue
@@ -49,8 +49,6 @@
            <span class="block">
              <lable>Description</lable>
                 <textarea
-                  id="message"
-                  name="message"
                   v-model="description"
                   rows="3"
                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
@@ -92,6 +90,7 @@ export default {
   },
   created() {
     this.highlightImages();
+    this.description=this.des;
   },
   data() {
     return {
